61bc3b9055472f50caf9b3ab46e4b4b411848868
EnginKosure <engin.kosure@cronos.be>
2020-08-27 15:37:43 +0200

    routes has been added

--- app.py
+++ app.py
@@ -2,5 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def head():
+    return 'Hello World!'
+
+
+@app.route('/second')
+def second():
+    return 'This is the second page'
+
+
+@app.route('/third/sub')
+def third():
+    return 'Sub route of third route'
+
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+    return f'Id of this page is {id}'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
