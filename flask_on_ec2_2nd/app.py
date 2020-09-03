f222ff5c4b8d89517bd6ded760679921b832053e
EnginKosure <engin.kosure@cronos.be>
2020-09-04 01:11:37 +0200

    flask with if statements explored

--- flask_on_ec2_2nd/app.py
+++ flask_on_ec2_2nd/app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index.html')
+@app.route('/')
 def head():
-    return render_template('index.html')
+    first = 'This is the first condition'
+    return render_template('index.html', message=first)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)  # for localhost
+    app.run(host='0.0.0.0', port=80)  # For production
